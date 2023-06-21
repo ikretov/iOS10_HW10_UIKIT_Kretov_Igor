b788b666ee8f47ab615f63cdf225f193c90d4c83
i_kretov <kretov.ig@mail.ru>
2023-06-22 00:07:23 +0900

    Initial Commit

---
+++ iOS10_HW10_UIKIT_Kretov_Igor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS10_HW10_UIKIT_Kretov_Igor
+//
+//  Created by Igor Kretov on 22.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
